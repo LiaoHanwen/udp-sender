91ecb5c0dfcfbd5ef7f89fdf5c6d7d2d946e29cf
LiaoHanwen <login@liaohanwen.com>
2018-06-14 11:24:28 +0800

    Initial commit

---
+++ udp sender.py
@@ -0,0 +1,26 @@
+# python3
+
+# udp sender
+# send a udp packet every time span
+# PACKET: packet content
+# TIME: time span
+# IP: ip address of the destination
+# PORT: port of the destination
+
+import socket
+import time
+
+PACKET = bytes("12345", encoding="utf-8")    #use bytes or encode with "bytes(string, encoding)"
+TIME = 1     #s
+
+IP = "192.168.137.255"
+PORT = 8090
+
+client=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  #create udp socket
+counter = 0     #counter for dispatch
+
+while(True):
+    counter += 1
+    client.sendto(PACKET, (IP,PORT))   #send message
+    print("counter:" + str(counter))
+    time.sleep(TIME)
