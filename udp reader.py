bf4aee2979861b138fd1bfcaf972cbbc36ca1faa
LiaoHanwen <login@liaohanwen.com>
2018-06-22 16:23:29 +0800

    bug fixed and format corrected

--- udp reader.py
+++ udp reader.py
@@ -1,15 +1,12 @@
 import socket
 import time
 
-TIME = 1     #s
-
 PORT = 8090
 
-server90=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  #create udp socket
-counter = 0     #counter for dispatch
-server90.bind(("0.0.0.0",PORT))
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  #create udp socket
+counter = 0     #counter for receive
+server.bind(("0.0.0.0",PORT))   #bind port
 while(True):
     counter += 1
-    addr, data = server90.recvfrom(1024)
+    data, addr = server.recvfrom(1024)
     print(str(counter) + ":", addr, data)
-    time.sleep(TIME)
